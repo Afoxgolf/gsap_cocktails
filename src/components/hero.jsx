3e452ed7492bd6c613e4a87456126a0d700e513a
Afoxgolf <austinwfox11@gmail.com>
2025-08-07 18:47:38 -0500

    Implement Hero Scroll animation

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
 import { SplitText } from 'gsap/all';
+import { useMediaQuery } from 'react-responsive';
 
 const Hero = () => {
+    const videoRef = useRef();
+
+    const isMobile = useMediaQuery({ maxWidth: 768 });
+
     useGSAP(() => {
-        const heroSplit = new SplitText('.title', { type: 'chars, words'});
+        const heroSplit = new SplitText('.title', { type: 'chars, words' });
 
         const paragraphSplit = new SplitText('.subtitle', { type: 'lines' });
 
@@ -35,48 +40,81 @@ const Hero = () => {
                 scrub: true,
             }
         })
-        .to('.right-leaf', { y: 200}, 0)
-        .to('.left-leaf', { y: -200}, 0)
+            .to('.right-leaf', { y: 200 }, 0)
+            .to('.left-leaf', { y: -200 }, 0)
+
+        const startValue = isMobile ? 'top 50%' : 'center 60%';
+        const endValue = isMobile ? '120% top' : 'bottom top';
+
+        if (videoRef.current) {
+            const tl = gsap.timeline({
+                scrollTrigger: {
+                    trigger: videoRef.current,
+                    start: startValue,
+                    end: endValue,
+                    scrub: true,
+                    pin: true,
+                }
+            });
+            videoRef.current.onloadedmetadata = () => {
+                tl.to(videoRef.current, {
+                    currentTime: videoRef.current.duration,
+                });
+            };
+        }
+
+
     }, []);
-  return (
-    <>
-        <section id='hero' className='noisy'>
-            <h1 className='title'>MOJITO</h1>
+    return (
+        <>
+            <section id='hero' className='noisy'>
+                <h1 className='title'>MOJITO</h1>
 
-            <img
-                src="/images/hero-left-leaf.png"
-                alt="left leaf"
-                className='left-leaf'
-            />
-              <img
-                  src="/images/hero-right-leaf.png"
-                  alt="right leaf"
-                  className='right-leaf'
-              />
+                <img
+                    src="/images/hero-left-leaf.png"
+                    alt="left leaf"
+                    className='left-leaf'
+                />
+                <img
+                    src="/images/hero-right-leaf.png"
+                    alt="right leaf"
+                    className='right-leaf'
+                />
 
-              <div className='body'>
-                <div className='content'>
-                    <div className='space-y-5 hidden md:block'>
-                        <p>Cool, Crisp, Classic</p>
-                        <p className='subtitle'>Sip the Spirit <br /> of Summer</p>
+                <div className='body'>
+                    <div className='content'>
+                        <div className='space-y-5 hidden md:block'>
+                            <p>Cool, Crisp, Classic</p>
+                            <p className='subtitle'>Sip the Spirit <br /> of Summer</p>
 
-                    </div>
+                        </div>
 
-                    <div className='view-cocktails'>
-                        <p className='subtitle'>
-                            Every cocktail on our menu is a blend of fresh ingredients and artisanal spirits, crafted to perfection by our expert mixologists.
+                        <div className='view-cocktails'>
+                            <p className='subtitle'>
+                                Every cocktail on our menu is a blend of fresh ingredients and artisanal spirits, crafted to perfection by our expert mixologists.
 
-                        </p>
-                        <a href='#cocktails' className='btn'>View Cocktails</a>
+                            </p>
+                            <a href='#cocktails' className='btn'>View Cocktails</a>
+                        </div>
+
                     </div>
 
                 </div>
+            </section>
 
-              </div>
-        </section>
-    
-    </>
-  )
+            <div className='video absolute inset-0'>
+                <video
+                    ref={videoRef}
+                    src='/videos/output.mp4'
+                    muted
+                    playsInline
+                    preload='auto'
+
+                />
+            </div>
+
+        </>
+    )
 }
 
 export default Hero
